83f485f057c4093b4fcc59d76052dac70c4378c9
Stefano <tepo@hotmail.it>
2021-03-01 15:39:15 +0100

    Mail: chiedo mail e creo lista

---
+++ mail/js/scripts.js
@@ -0,0 +1,9 @@
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+// 1. chiedo all'utente la email
+var mail = prompt("Inserisci la tua mail");
+
+// 2. definisco lista email
+var lista = ["andrea@gmail.com", "franco@yahoo.it", "alberto@hotmail.it", "stefano@outlook.com", "giulia@gmail.com"];
