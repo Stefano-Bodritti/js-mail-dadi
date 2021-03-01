98d22ac073f16c2dc89386fc3cfc5840c34823d8
Stefano <tepo@hotmail.it>
2021-03-01 16:01:27 +0100

    Mail: verifico corrispondenza e stampo messaggio

--- mail/js/scripts.js
+++ mail/js/scripts.js
@@ -7,3 +7,18 @@ var mail = prompt("Inserisci la tua mail");
 
 // 2. definisco lista email
 var lista = ["andrea@gmail.com", "franco@yahoo.it", "alberto@hotmail.it", "stefano@outlook.com", "giulia@gmail.com"];
+var corrispondenza = 0;
+
+// 3. controllo se la mail inserita è in lista, se lo è fisso corrispondenza=1
+for (var i = 0; i < lista.length; i++) {
+  if (mail == lista[i]) {
+    var corrispondenza = 1;
+  }
+}
+
+// 4. stampo messaggio appropriato
+if (corrispondenza == 1) {
+  alert("La tua mail è in lista!");
+} else {
+  alert("Mi dispiace, la tua mail non è in lista!");
+}
